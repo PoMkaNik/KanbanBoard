9d1c56aa561aeb4a213bb91184817bad1abae27e
Roman Dubovik <ip.romandubovik@gmail.com>
2020-12-09 12:08:02 +0300

    sidebar hide and toggle behaviour for devices below lg added

---
+++ dist/js/main.js
@@ -0,0 +1,6 @@
+// const sidebarToggler = document.getElementById('js--sidebar-toggler');
+
+function toggleSidebar() {
+  const sidebar = document.getElementById('js--sidebar');
+  sidebar.classList.toggle('hidden');
+}
